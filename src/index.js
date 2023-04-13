b64ea620ff1a615be68bf4f2dc30ebebe9dc73c9
EvanLesnefsky <112042025+EvanLesnefsky@users.noreply.github.com>
2023-04-13 14:18:26 -0600

    login test almost working

--- src/index.js
+++ src/index.js
@@ -73,6 +73,59 @@ app.get('/welcome', (req, res) => {
   });
 
 // TODO - Login and Register
+app.get('/', (req, res) => {
+  return res.redirect('/login');
+});
+
+app.get('/register', (req, res) => {
+  return res.render('pages/register');
+});
+
+// Register
+app.post('/register', async (req, res) => {
+  //hash the password using bcrypt library
+  const hash = await bcrypt.hash(req.body.password, 10);
+  // To-DO: Insert username and hashed password into 'users' table
+  const query = "INSERT INTO users (username, password) VALUES ($1, $2);";
+  db.any(query, [
+    req.body.username,
+    hash,
+  ])
+    .then(function () {
+      return res.redirect('/login');
+    })
+    .catch(function () {
+      return res.render('pages/register', { message: 'This account has already been registered' });
+    });
+});
+
+app.get("/login", (req, res) => {
+  return res.render("pages/login");
+});
+
+app.post('/login', async (req, res) => {
+  const pwInDB = await db.any('SELECT * FROM users WHERE users.username = $1', [
+    req.body.username])
+    .catch(function () {
+      return res.render('pages/login', { message: 'Database request failed' });
+    });
+  if (pwInDB.length > 0) {
+    const match = await bcrypt.compare(req.body.password, pwInDB[0].password);
+    if (match) {
+      req.session.user = {
+        api_key: process.env.API_KEY,
+      };
+      req.session.save();
+      return res.redirect('/discover');
+    }
+    else {
+      return res.render('pages/login', { message: 'Incorrect username or password' });
+    }
+  }
+  else {
+    return res.redirect('/register');
+  }
+});
 
 const auth = (req, res, next) => {
   if (!req.session.user) {
