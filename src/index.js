2d46cc93143b4ae5e36b86877ed1e1b6f0823adb
Warren Fu <whfu2004@gmail.com>
2023-04-21 10:50:51 -0600

    Testing API keys

--- src/index.js
+++ src/index.js
@@ -2,27 +2,32 @@
 // <!-- Section 1 : Import Dependencies -->
 // *****************************************************
 
-const express = require('express'); // To build an application server or API
+const express = require("express"); // To build an application server or API
 const app = express();
-const pgp = require('pg-promise')(); // To connect to the Postgres DB from the node server
-const bodyParser = require('body-parser');
-const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
-const bcrypt = require('bcrypt'); //  To hash passwords
-const axios = require('axios'); // To make HTTP requests from our server. We'll learn more about it in Part B.
-const { json } = require('body-parser');
-
+const pgp = require("pg-promise")(); // To connect to the Postgres DB from the node server
+const bodyParser = require("body-parser");
+const session = require("express-session"); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
+const bcrypt = require("bcrypt"); //  To hash passwords
+const axios = require("axios"); // To make HTTP requests from our server. We'll learn more about it in Part B.
+const { json } = require("body-parser");
 
 const user = {
   username: undefined,
+  property_id: undefined,
+  status_id: undefined,
   password: undefined,
-}
+  email: undefined,
+  phone_number: undefined,
+  gender: undefined,
+  birthdate: undefined,
+};
 // *****************************************************
 // <!-- Section 2 : Connect to DB -->
 // *****************************************************
 
 // database configuration
 const dbConfig = {
-  host: 'db', // the database server
+  host: "db", // the database server
   port: 5432, // the database port
   database: process.env.POSTGRES_DB, // the database name
   user: process.env.POSTGRES_USER, // the user account to connect with
@@ -33,19 +38,19 @@ const db = pgp(dbConfig);
 
 // test your database
 db.connect()
-  .then(obj => {
-    console.log('Database connection successful'); // you can view this message in the docker compose logs
+  .then((obj) => {
+    console.log("Database connection successful"); // you can view this message in the docker compose logs
     obj.done(); // success, release the connection;
   })
-  .catch(error => {
-    console.log('ERROR:', error.message || error);
+  .catch((error) => {
+    console.log("ERROR:", error.message || error);
   });
 
 // *****************************************************
 // <!-- Section 3 : App Settings -->
 // *****************************************************
 
-app.set('view engine', 'ejs'); // set the view engine to EJS
+app.set("view engine", "ejs"); // set the view engine to EJS
 app.use(bodyParser.json()); // specify the usage of JSON for parsing request body.
 
 // initialize session variables
@@ -68,54 +73,57 @@ app.use(
 
 // TODO - Include your API routes here
 
-app.get('/welcome', (req, res) => {
-    res.json({status: 'success', message: 'Welcome!'});
-  });
+app.get("/welcome", (req, res) => {
+  res.json({ status: "success", message: "Welcome!" });
+});
 
 // TODO - Login and Register
 
-
-app.get('/', (req, res) => {
-  res.redirect('/explore');
+app.get("/", (req, res) => {
+  res.redirect("/explore");
 });
 
-app.get('/explore', (req, res) => {
-  res.render('pages/explore',{});
+app.get("/explore", (req, res) => {
+  res.render("pages/explore", {});
 });
 
-app.get('/register', (req, res) => {
-  res.render('pages/register',{});
+app.get("/register", (req, res) => {
+  res.render("pages/register", {});
 });
 
 // Register
-app.post('/register', async (req, res) => {
+app.post("/register", async (req, res) => {
   //hash the password using bcrypt library
   const hash = await bcrypt.hash(req.body.password, 10);
 
   // To-DO: Insert username and hashed password into 'users' table
-  if (hash.err || req.body.username === "" || req.body.password === "" || req.body.email === ""){
-    res.redirect('/register');
-  }
-  else{
-      var query = `INSERT INTO users(username, password, email) VALUES ('${req.body.username}', '${hash}', '${req.body.email}');`;
-      
-      db.any(query)
+  if (
+    hash.err ||
+    req.body.username === "" ||
+    req.body.password === "" ||
+    req.body.email === ""
+  ) {
+    res.redirect("/register");
+  } else {
+    var query = `INSERT INTO users(username, password, email) VALUES ('${req.body.username}', '${hash}', '${req.body.email}');`;
+
+    db.any(query)
       .then(function (data) {
-        res.redirect('/login');
+        res.redirect("/login");
       })
       .catch(function (err) {
         console.log(err);
-          res.render('pages/register',{});
+        res.render("pages/register", {});
       });
   }
 });
 
-app.get('/login', (req, res) => {
+app.get("/login", (req, res) => {
   // res.json({status: 'success', message: 'Works!'});
-  res.render('pages/login',{});
+  res.render("pages/login", {});
 });
 
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
   const query = `SELECT * FROM users WHERE username = 'abcd';`;
@@ -123,31 +131,52 @@ app.post('/login', async (req, res) => {
     .then((data) => {
       user.username = data.username;
       user.password = data.password;
-      if (bcrypt.compare('abcd1234', user.password)){
+      if (bcrypt.compare("abcd1234", user.password)) {
         req.session.user = user;
         req.session.save();
         console.log("works!", user.password); //prints works! if we are able to log in.
         res.redirect("/");
-        
-      }
-      else{
+      } else {
         console.log("Incorrect username or password.");
-        res.render('/views/pages/login.ejs',{})
+        res.render("/views/pages/login.ejs", {});
       }
     })
     .catch((err) => {
       console.log(err);
-      res.redirect('/register');
+      res.redirect("/register");
     });
 });
 
+//Listings page
+app.get("/listing", (req, res) => {
+  // pass in property_id
+  const axios = require("axios");
 
+  const options = {
+    method: "GET",
+    url: "https://realty-in-us.p.rapidapi.com/properties/v3/get-photos",
+    params: { property_id: data},
+    headers: {
+      "X-RapidAPI-Key": "51110bf831mshd401637aba1666dp184555jsnf920fb6f82f1",
+      "X-RapidAPI-Host": "realty-in-us.p.rapidapi.com",
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+      res.render("pages/login", {});
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+});
 
 app.get("/logout", (req, res) => {
   req.session.destroy();
   res.render("pages/login");
 });
-
 
 const auth = (req, res, next) => {
   if (!req.session.user) {
@@ -165,4 +194,4 @@ app.use(auth);
 // *****************************************************
 // starting the server and keeping the connection open to listen for more requests
 module.exports = app.listen(3000);
-console.log('Server is listening on port 3000');
+console.log("Server is listening on port 3000");
